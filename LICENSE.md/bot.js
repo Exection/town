9dbbd5bb4b37a6ce6290d404266a0cbcf4048ebf
Exection <42671740+Exection@users.noreply.github.com>
2018-10-11 18:27:09 +0200

    Create bot.js

---
+++ LICENSE.md/bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '!'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
+
+
+
+
+client.login('process.env.BOT_TOKEN');
+
+
+client.on('ready',async () => {
+  let GUILDID = '480895922281381898'; // اي دي السيرفر
+  let CHANNELID = '480929666971205648'; // اي دي الروم
+  voiceStay(GUILDID, CHANNELID);
+  function voiceStay(guildid, channelid) {
+    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
+    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
+
+    let guild = client.guilds.get(guildid);
+    let channel = guild.channels.get(channelid);
+
+    if(channel.type === 'voice') {
+      channel.join().catch(e => {
+        console.log(`Failed To Join :: ${e.message}`);
+      });
+    } else {
+      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
+    }
+  }
+});
